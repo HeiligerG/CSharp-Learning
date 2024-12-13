c5e63e14c334248aa457fffd374f730483f01d07
HeiligerG <gggigi111@gmail.com>
2024-12-13 09:43:22 +0100

    Start PJ and implement Mainfunction

---
+++ Schule/GalaxieExplorer/GalaxieExplorer/GalaxieExplorer/Speilsteuerung.cs
@@ -0,0 +1,29 @@
+namespace GalaxieExplorer;
+
+public class Speilsteuerung
+{
+    public void zeigeHauptmenu()
+    {
+        Console.WriteLine("Willkommen zum Hauptmenu:");
+    }
+
+    public void verarbeiteEingabe()
+    {
+        
+    }
+
+    public void zeigeInventar()
+    {
+        
+    }
+
+    public void ziegeStatus()
+    {
+        
+    }
+
+    public void zeigeHilfe()
+    {
+        
+    }
+}
